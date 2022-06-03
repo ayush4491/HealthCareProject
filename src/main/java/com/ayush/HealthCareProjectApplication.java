683bd4e16e268434affbfbd3580ba20ddb50bfc1
unknown <ayush4491@gmail.com>
2022-06-03 19:04:03 +0530

    first commit

---
+++ src/main/java/com/ayush/HealthCareProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ayush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCareProjectApplication.class, args);
+	}
+
+}
